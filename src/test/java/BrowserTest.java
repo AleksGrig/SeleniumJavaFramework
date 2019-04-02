e7dab018ef18fc6839a0c54aaec46c0c522757d5
Aleksey Grigoruk <ullitap@gmail.com>
2019-04-02 13:59:21 +0300

    New Selenium project

---
+++ src/test/java/BrowserTest.java
@@ -0,0 +1,14 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class BrowserTest {
+
+	public static void main(String[] args) {
+		
+		String projectPath = System.getProperty("user.dir");
+				
+		System.setProperty("webdriver.gecko.driver", projectPath + "\\drivers\\geckodriver\\geckodriver.exe");
+		WebDriver driver = new FirefoxDriver();
+		driver.get("http://seleniumhq.org/");
+	}
+}
