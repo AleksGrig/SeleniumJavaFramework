2043712ffb344c939da1d519b5a70d45ffab3485
Aleksey Grigoruk <ullitap@gmail.com>
2019-04-04 08:44:24 +0300

    WebDriver Manager added.

--- src/test/java/BrowserTest.java
+++ src/test/java/BrowserTest.java
@@ -3,9 +3,18 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class BrowserTest {
 
 	public static void main(String[] args) {
+		
+		WebDriverManager.chromedriver().setup();
+		//WebDriverManager.firefoxdriver().setup();
+		//WebDriverManager.operadriver().setup();
+		//WebDriverManager.phantomjs().setup();
+		//WebDriverManager.edgedriver().setup();
+		//WebDriverManager.iedriver().setup();
 		
 		String projectPath = System.getProperty("user.dir");
 		
@@ -13,11 +22,11 @@ public class BrowserTest {
 		//WebDriver driver = new FirefoxDriver();
 		
 		//System.setProperty("webdriver.chrome.driver", projectPath + "/drivers/chromedriver/chromedriver.exe");
-		//WebDriver driver = new ChromeDriver();
+		WebDriver driver = new ChromeDriver();
 		
-		System.setProperty("webdriver.ie.driver", projectPath + "/drivers/iedriver/IEDriverServer.exe");
-		WebDriver driver = new InternetExplorerDriver();
-		
+		//System.setProperty("webdriver.ie.driver", projectPath + "/drivers/iedriver/IEDriverServer.exe");
+		//WebDriver driver = new InternetExplorerDriver();
+	
 		driver.get("http://seleniumhq.org");
 	}
 }
