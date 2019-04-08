6171b10ff655e923c71d909bdde831e7d93689b0
Aleksey Grigoruk <ullitap@gmail.com>
2019-04-08 10:35:38 +0300

    Implementing of POM.

--- src/test/java/test/GoogleSearchTest.java
+++ src/test/java/test/GoogleSearchTest.java
@@ -7,7 +7,11 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class Test1_GoogleSearch {
+import pages.GoogleSearchPage;
+
+public class GoogleSearchTest {
+	
+	private static WebDriver driver = null;
 
 	public static void main(String[] args) {
 		googleSearch();
@@ -16,19 +20,20 @@ public class Test1_GoogleSearch {
 	public static void googleSearch() {
 		
 		WebDriverManager.chromedriver().setup();
-		WebDriver driver = new ChromeDriver();
+		driver = new ChromeDriver();
 		
 		//goto google.com
 		driver.get("http://google.com");
 		
 		//enter text in search textbox
-		driver.findElement(By.name("q")).sendKeys("Big brother watches you...");
+		//driver.findElement(By.name("q")).sendKeys("Big brother watches you...");
+		GoogleSearchPage.textbox_search(driver).sendKeys("Big brother watches you...");
 		
 		//click on search button
 		//driver.findElement(By.name("btnk")).click();
 		//driver.findElement(By.name("q")).sendKeys(Keys.RETURN);
-		driver.findElement(By.xpath("//div[@class='FPdoLc VlcLAe']//input[@value='Поиск в Google']")).click();
-		
+		//driver.findElement(By.xpath("//div[@class='FPdoLc VlcLAe']//input[@value='Поиск в Google']")).click();
+		GoogleSearchPage.button_search(driver).click();
 		System.out.println("Test completed successfully");
 	}
 }
